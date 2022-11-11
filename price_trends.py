eb53f67f17b811f1c19d67702e4712cc3b15e32c
bijoy <bijoy.thomas95@gmail.com>
2022-11-11 16:00:22 -0500

    adding stream files

--- price_trends.py
+++ price_trends.py
@@ -16,7 +16,7 @@ def get_pct_change(df, col_name):
 
 def resample(asset_type: str, abbrev: str):
     filtered = master.query("HomeType == '{}'".format(asset_type))
-    resampled = filtered.resample('W-Mon', on='LastUpdated')\
+    resampled = filtered.resample('W', on='LastUpdated')\
         .agg({'ListedPrice': 'mean', 'zpid': 'nunique'})\
         .astype(int)\
         .reset_index() \
@@ -51,7 +51,7 @@ s3_client.download_file('listingszillow2022', 'listings_master.db', 'listings_ma
 db = 'listings_master.db'
 conn = sqlite3.connect(db)
 
-master = pd.read_sql('select distinct ListedPrice, HomeType, zpid, LastUpdated from "{}"'.format(db), conn)
+master = pd.read_sql('select distinct ListedPrice, HomeType, zpid, LastUpdated from "{}" where LastUpdated >= "06/19/22"'.format(db), conn)
 master['LastUpdated'] = pd.to_datetime(master['LastUpdated'])
 master = master.sort_values(by='LastUpdated')
 
